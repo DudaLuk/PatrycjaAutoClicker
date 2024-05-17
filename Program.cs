f22b425b6483a069695b9b1fdeaced72e86ad975
lukasz <lukasz@huber.net.pl>
2024-05-17 12:36:37 +0200

    dodanie selimenium

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ConsoleApp1
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Inicjalizacja przeglądarki Chrome
+            IWebDriver driver = new ChromeDriver();
+
+            try
+            {
+                // Przejdź do strony Google
+                driver.Navigate().GoToUrl("https://www.google.pl");
+
+                // Znajdź pole wyszukiwania
+                IWebElement searchBox = driver.FindElement(By.Name("q"));
+
+                // Wpisz zapytanie do pola wyszukiwania
+                searchBox.SendKeys("Selenium WebDriver");
+
+                // Znajdź przycisk "Szukaj" i kliknij go
+                searchBox.Submit();
+
+                // Czekaj kilka sekund, aby zobaczyć wyniki (opcjonalnie)
+                System.Threading.Thread.Sleep(5000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd: {e.Message}");
+            }
+            finally
+            {
+                // Zamknij przeglądarkę
+                driver.Quit();
+            }
+
         }
     }
 }
