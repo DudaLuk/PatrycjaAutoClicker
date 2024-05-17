e7b02fa191478e93ac00637137d8ed869fce8ae4
lukasz <lukasz@huber.net.pl>
2024-05-17 14:42:43 +0200

    pętla

--- Program.cs
+++ Program.cs
@@ -8,35 +8,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Inicjalizacja przeglądarki Chrome
-            IWebDriver driver = new OpenQA.Selenium.Edge.EdgeDriver();
-
-            try
+            while (true)
             {
-                // Przejdź do strony Google
-                driver.Navigate().GoToUrl("https://www.shecodes.io/contests/urban-garden-challenge/contest_entries/patrycja-oosthuizen-polish-b3d6f284-7841-4f83-869e-66fac50854fd/preview");
+                Browser_job();
+                System.Threading.Thread.Sleep(TimeSpan.FromMinutes(1));
 
-                // Znajdź pole wyszukiwania
-                //IWebElement searchBox = driver.FindElement(By.Name("q"));
-                IWebElement webElement = driver.FindElement(By.ClassName("btn-secondary"));
-                webElement.Click();
-                // Wpisz zapytanie do pola wyszukiwania
-                //searchBox.SendKeys("Selenium WebDriver");
-
-                // Znajdź przycisk "Szukaj" i kliknij go
-                //searchBox.Submit();
-
-                // Czekaj kilka sekund, aby zobaczyć wyniki (opcjonalnie)
-                System.Threading.Thread.Sleep(5000);
             }
-            catch (Exception e)
+
+
+
+            static void Browser_job()
             {
-                Console.WriteLine($"Wystąpił błąd: {e.Message}");
-            }
-            finally
-            {
-                // Zamknij przeglądarkę
-                driver.Quit();
+                // Inicjalizacja przeglądarki Chrome
+                IWebDriver driver = new OpenQA.Selenium.Edge.EdgeDriver();
+
+                try
+                {
+                    // Przejdź do strony Google
+                    driver.Navigate().GoToUrl("https://www.shecodes.io/contests/urban-garden-challenge/contest_entries/patrycja-oosthuizen-polish-b3d6f284-7841-4f83-869e-66fac50854fd/preview");
+
+                    // Znajdź pole wyszukiwania
+                    //IWebElement searchBox = driver.FindElement(By.Name("q"));
+                    IWebElement webElement = driver.FindElement(By.ClassName("btn-secondary"));
+                    webElement.Click();
+                    // Wpisz zapytanie do pola wyszukiwania
+                    //searchBox.SendKeys("Selenium WebDriver");
+
+                    // Znajdź przycisk "Szukaj" i kliknij go
+                    //searchBox.Submit();
+
+                    // Czekaj kilka sekund, aby zobaczyć wyniki (opcjonalnie)
+                    System.Threading.Thread.Sleep(5000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Wystąpił błąd: {e.Message}");
+                }
+                finally
+                {
+                    // Zamknij przeglądarkę
+                    driver.Quit();
+                }
             }
 
         }
